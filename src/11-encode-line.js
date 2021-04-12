f79baa3b80f4fb978921a89b08877477c85fdc31
Original-pokemon <eremkin-02@mail.ru>
2021-04-13 01:55:02 +0300

    feat: add 10, 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,21 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const result = [];
+  let counter = 1;
+  for (let i = 0; i < str.length; i++) {
+    const e = str[i];
+    if (e === str[i + 1]) {
+      counter++;
+    } else if (counter > 1) {
+      result.push(`${counter}${e}`);
+      counter = 1;
+    } else {
+      result.push(`${e}`);
+    }
+  }
+  return result.join('');
 }
 
 module.exports = encodeLine;
