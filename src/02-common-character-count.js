1c397bec45379237a73803f87b5d388cce7dd455
Original-pokemon <eremkin-02@mail.ru>
2021-04-13 00:04:33 +0300

    feat: add 02, 03, 15

--- src/02-common-character-count.js
+++ src/02-common-character-count.js
@@ -9,8 +9,21 @@
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new Error('Not implemented');
+function getCommonCharacterCount(s1, s2) {
+  const arr1 = s1.split('');
+  const arr2 = s2.split('');
+
+  const result = arr1.reduce((accum, value) => {
+    let sum = accum;
+    const index = arr2.indexOf(value);
+    if (index !== -1) {
+      sum += 1;
+      arr2.splice(index, 1);
+    }
+    return sum;
+  }, 0);
+
+  return result;
 }
 
 module.exports = getCommonCharacterCount;
--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,8 +9,20 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+function sortByHeight(arr) {
+  const sortArr = arr.filter((e) => e > 0).sort((a, b) => a - b);
+  const result = [];
+
+  let counter = 0;
+
+  arr.forEach((element) => {
+    if (element === -1) result.push(element);
+    else {
+      result.push(sortArr[counter]);
+      counter++;
+    }
+  });
+  return result;
 }
 
 module.exports = sortByHeight;
